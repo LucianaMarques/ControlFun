9129c8d6292185b8e02cdaef0b3e7046dbc2e7c9
lucianamarques <lucianadacostamarques@gmail.com>
2018-11-26 11:43:48 -0200

    added slider

--- ControlFun/ViewController.swift
+++ ControlFun/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
     
     @IBOutlet weak var numberField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
     
     @IBAction func textFiedDoneEditind(sender: UITextField)
@@ -25,6 +27,10 @@ class ViewController: UIViewController {
     @IBAction func onTapGestureRecognized(_ sender: Any) {
         nameField.resignFirstResponder()
         numberField.resignFirstResponder()
+    }
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = "\(lroundf(sender.value))"
+        
     }
 }
 
