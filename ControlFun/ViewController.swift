d5b7866332de4633b56fced6d6a3912aaa84284b
lucianamarques <lucianadacostamarques@gmail.com>
2018-11-27 16:05:31 -0200

    slider, button and switches not working properly

--- ControlFun/ViewController.swift
+++ ControlFun/ViewController.swift
@@ -41,7 +41,33 @@ class ViewController: UIViewController {
         rightSwitch.setOn(setting, animated: true)
     }
     @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?",message:nil, preferredStyle: .actionSheet)
+        let yesAction = UIAlertAction(title: "Yes, I'm sure!",style: .destructive, handler: { action in let msg = self.nameField.text!.isEmpty
+            ? "You can breathe easy, everything went OK."
+            : "You can breathe easy, \(String(describing: self.nameField.text)),"
+            + "everything went OK."
+            let controller2 = UIAlertController(
+                title:"Something Was Done",
+                message: msg, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "Phew!",
+                                             style: .cancel,
+                                             handler: nil)
+            controller2.addAction(cancelAction)
+            self.present(controller2, animated: true,
+                         completion: nil)
+        })
+        let noAction = UIAlertAction(title: "No way!",
+                                     style: .cancel, handler: nil)
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        if let ppc = controller.popoverPresentationController {
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+        present(controller, animated: true, completion: nil)
+        
     }
+   
     @IBAction func toggleControls(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {  // "Switches" is selected
             leftSwitch.isHidden = false
@@ -52,7 +78,6 @@ class ViewController: UIViewController {
             rightSwitch.isHidden = true
             doSomethingButton.isHidden = false
         }
-        
     }
 }
 
